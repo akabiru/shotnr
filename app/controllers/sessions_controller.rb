a3cabe8670d6626a1c3a4afd5a7ff6b2069a522d
Austin Kabiru <austin.kabiru@andela.com>
2016-04-28 19:32:47 +0300

    feat(anonymous user): modify landing page view
     - add logout user route
    
    [Finishes #118074983]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    logout
+    redirect_to root_url
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'urls#index'
   resources :urls
-  get '/:short_url' => 'urls#redirect'
+
   get '/auth/:provider/callback' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/:short_url' => 'urls#redirect'
+
 end
