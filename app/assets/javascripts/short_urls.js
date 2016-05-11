f91acc889086706c83e63739ef48ff745b8c5e1d
Austin Kabiru <austin.kabiru@andela.com>
2016-05-11 23:23:23 +0300

    replace ajaxSetup with beforeSend

--- app/assets/javascripts/short_urls.js
+++ app/assets/javascripts/short_urls.js
@@ -16,15 +16,13 @@ $(function() {
         validateVanityString('vanity', 'Your custom url is too short.', 'success', 'danger')
       } else {
         validateVanityString('vanity', '', 'danger', 'success')
-        $.ajaxSetup({
-          headers: {
-            'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-          }
-        })
         $.ajax({
           method: 'GET',
           url: '/search/' + this.value,
           dataType: 'json',
+          beforeSend: function(xhr) {
+            xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+          },
           success: function(data) {
             if (data.exists) {
               validateVanityString('vanity', 'sorry, that url is already taken', 'success', 'warning')
