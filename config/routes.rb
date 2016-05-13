768d2c2014267c7b120da418c307c6ceeae46344
Austin Kabiru <austin.kabiru@andela.com>
2016-05-13 15:42:09 +0300

    set js route on url post

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root 'original_urls#index'
-  resources :original_urls
-  resources :short_urls
+
+  post 'original_urls' => 'original_urls#create', constraints: { format: 'js' }
 
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   get '/search/:vanity_string_' => 'short_urls#check_vanity_string', constraints: { format: 'json' }
+
+  resources :original_urls
+  resources :short_urls
+
   get '/:vanity_string' => 'original_urls#redirect_to_original_url'
 
 end
