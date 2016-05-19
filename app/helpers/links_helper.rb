f41a9a43bc0ab617cefaa71e79fc4b6e6a526374
Austin Kabiru <austin.kabiru@andela.com>
2016-05-19 07:50:35 +0300

    add full link

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -2,4 +2,8 @@ module LinksHelper
   def shotlink(link)
     "#{root_url.sub(/www./, '')}#{link.vanity_string}"
   end
+
+  def full_shotlink(link)
+    "#{root_url}#{link.vanity_string}"
+  end
 end
--- spec/helpers/links_helper_spec.rb
+++ spec/helpers/links_helper_spec.rb
@@ -5,6 +5,16 @@ RSpec.describe LinksHelper, type: :helper do
     it "returns shotlink without www" do
       link = create(:link)
       full_link = shotlink(link)
+      expect(
+        full_link
+      ).to eq("#{root_url.sub(/www./, '')}#{link.vanity_string}")
+    end
+  end
+
+  describe "#full_shotlink" do
+    it "returns full shotlink" do
+      link = create(:link)
+      full_link = shotlink(link)
       expect(full_link).to eq("#{root_url}#{link.vanity_string}")
     end
   end
